fd78caee90ec56d5ad6c9a6b3e1670c5617c7c48
jothi <jothi@vishgyana.com>
2021-08-17 14:35:39 +0530

    Tab Correction

--- pages/scratchcard/_campaign_member_id.vue
+++ pages/scratchcard/_campaign_member_id.vue
@@ -190,32 +190,23 @@
         </svg>
       </div>
     </div>
-    <div class="grid h-90 grid-cols">
+    <div class="grid grid-cols-none items-center h-screen flex">
       <div class="main_content">
-        <h2 class="ml10 text-center font-bold text-3xl mb-2">
+        <div v-if="isShowsurprisesection">
+          <h2 class="ml10 text-center font-bold text-3xl mb-2">
           <span class="text-wrapper">
-            <span class="letters">Surprise!</span>
+            <span class="letters"
+                  data-aos="fade-in"
+                  data-aos-easing="ease-in-out"
+                  data-aos-duration="0"
+                  data-aos-delay="1600">Surprise!</span>
           </span>
-        </h2>
-        <h1 class="ml6 text-center font-bold text-3xl mb-2">
-          <span class="text-wrapper">
-            <span class="letters">Surprise!</span>
-          </span>
-        </h1>
-        <h1 class="ml15 text-center font-bold text-3xl mb-2">
-          <span class="word">Surprise!</span>
-        </h1>
-        <h1 class="ml16 text-center font-bold text-3xl mb-2">Surprise!</h1>
-        <p
-          class="text-center font-normal text-base mb-8"
-          v-if="isShowsurprisesection"
-          data-aos="fade"
-          data-aos-easing="ease-in-out"
-          data-aos-duration="300"
-          data-aos-delay="2400"
-        >
-          Scratch the card to unveil them.
-        </p>
+          </h2>
+          <p
+            class="text-center font-normal text-base mb-8">
+            Scratch the card to unveil them.
+          </p>
+        </div>
         <Vuescratchcard
           data-aos="fade"
           data-aos-easing="ease-in-out"
@@ -277,65 +268,9 @@ export default {
         "<span class='letter'>$&</span>"
       );
 
-      // Wrap every letter in a span
-      var textWrapper = document.querySelector(".ml6 .letters");
-      textWrapper.innerHTML = textWrapper.textContent.replace(
-        /\S/g,
-        "<span class='letter'>$&</span>"
-      );
-
-      // Wrap every letter in a span
-      var textWrapper = document.querySelector(".ml16");
-      textWrapper.innerHTML = textWrapper.textContent.replace(
-        /\S/g,
-        "<span class='letter'>$&</span>"
-      );
-
       this.$anime
         .timeline({ loop: true })
-        .add({
-          targets: ".ml16 .letter",
-          translateY: [-100, 0],
-          easing: "easeOutExpo",
-          duration: 1400,
-          delay: (el, i) => 30 * i
-        })
-        .add({
-          targets: ".ml16",
-          opacity: 0,
-          duration: 1000,
-          easing: "easeOutExpo",
-          delay: 1000
-        })
-        .add({
-          targets: ".ml15 .word",
-          scale: [14, 1],
-          opacity: [0, 1],
-          easing: "easeOutCirc",
-          duration: 800,
-          delay: (el, i) => 800 * i
-        })
-        .add({
-          targets: ".ml15",
-          opacity: 0,
-          duration: 1000,
-          easing: "easeOutExpo",
-          delay: 1000
-        })
-        .add({
-          targets: ".ml6 .letter",
-          translateY: ["1.1em", 0],
-          translateZ: 0,
-          duration: 750,
-          delay: (el, i) => 50 * i
-        })
-        .add({
-          targets: ".ml6",
-          opacity: 0,
-          duration: 1000,
-          easing: "easeOutExpo",
-          delay: 1000
-        })
+
         .add({
           targets: ".ml10 .letter",
           rotateY: [-90, 0],
