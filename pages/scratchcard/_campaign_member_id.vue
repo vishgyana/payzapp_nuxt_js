5a1ef5f7429edc0b6ee6f10c32763e582dceac50
jothivgts <jothi@vishgyana.com>
2021-08-26 12:13:48 +0530

    Latest

--- pages/scratchcard/_campaign_member_id.vue
+++ pages/scratchcard/_campaign_member_id.vue
@@ -6,7 +6,7 @@
           data-aos="fade"
           data-aos-easing="ease-in-out"
           data-aos-duration="300"
-          data-aos-delay="300"
+          data-aos-delay="100"
         />
       </div>
       <div class="col-end-7">
@@ -14,7 +14,7 @@
           data-aos="fade"
           data-aos-easing="ease-in-out"
           data-aos-duration="300"
-          data-aos-delay="300"
+          data-aos-delay="100"
         />
       </div>
     </div>
